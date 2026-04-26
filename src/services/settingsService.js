4014fc6521fd5114a7fbd3575ebd4ee7a50d1c60
devjpedro <joao.seco@sampa.tech>
2026-04-25 23:08:59 -0300

    feat: usar imagens locais em vez de URLs externas

--- src/services/settingsService.js
+++ src/services/settingsService.js
@@ -1,37 +1,44 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const settingsFilePath = path.join(__dirname, '..', '..', 'settings.json');
+const settingsFilePath = path.join(__dirname, "..", "..", "settings.json");
 
 const defaultSettings = {
-  imageUrls: {
-    green: 'BAPIbL4.jpeg',
-    yellow: 'njoarXh.jpeg',
-    red: 'zS2yDav.jpeg'
-  },
-  phrases: {
-    entradaConfirmada: "✅Entry confirmed✅",
-    buscarAte: "Target up to:",
-    plataformaAbaixo: "🖥 Platform below~👇",
-    entrarNaCasa: "✈Join the house✈"
-  }
+	imageUrls: {
+		green: "BAPIbL4.png",
+		yellow: "njoarXh.png",
+		red: "zS2yDav.png",
+	},
+	phrases: {
+		entradaConfirmada: "✅Entry confirmed✅",
+		buscarAte: "Target up to:",
+		plataformaAbaixo: "🖥 Platform below~👇",
+		entrarNaCasa: "✈Join the house✈",
+	},
 };
 
 /**
  * @returns {typeof defaultSettings}
  */
 function getSettings() {
-  if (!fs.existsSync(settingsFilePath)) {
-    fs.writeFileSync(settingsFilePath, JSON.stringify(defaultSettings, null, 2), 'utf8');
-    return defaultSettings;
-  }
-  try {
-    const settings = JSON.parse(fs.readFileSync(settingsFilePath, 'utf8'));
-    return settings;
-  } catch (error) {
-    console.error("Failed to read settings file. Falling back to defaults.", error);
-    return defaultSettings;
-  }
+	if (!fs.existsSync(settingsFilePath)) {
+		fs.writeFileSync(
+			settingsFilePath,
+			JSON.stringify(defaultSettings, null, 2),
+			"utf8",
+		);
+		return defaultSettings;
+	}
+	try {
+		const settings = JSON.parse(fs.readFileSync(settingsFilePath, "utf8"));
+		return settings;
+	} catch (error) {
+		console.error(
+			"Failed to read settings file. Falling back to defaults.",
+			error,
+		);
+		return defaultSettings;
+	}
 }
 
 /**
@@ -40,17 +47,21 @@ function getSettings() {
  * @param {string} value
  */
 function updateSetting(mainKey, subKey, value) {
-  const settings = getSettings();
-  if (settings[mainKey] && typeof settings[mainKey][subKey] !== 'undefined') {
-    settings[mainKey][subKey] = value;
-    fs.writeFileSync(settingsFilePath, JSON.stringify(settings, null, 2), 'utf8');
-    console.log(`Setting updated: ${mainKey}.${subKey}`);
-    return true;
-  }
-  return false;
+	const settings = getSettings();
+	if (settings[mainKey] && typeof settings[mainKey][subKey] !== "undefined") {
+		settings[mainKey][subKey] = value;
+		fs.writeFileSync(
+			settingsFilePath,
+			JSON.stringify(settings, null, 2),
+			"utf8",
+		);
+		console.log(`Setting updated: ${mainKey}.${subKey}`);
+		return true;
+	}
+	return false;
 }
 
 module.exports = {
-  getSettings,
-  updateSetting
+	getSettings,
+	updateSetting,
 };
